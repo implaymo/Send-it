fb761c4bbb19cdb8a068ed48e6f59e57d5f98a61
implaymo <goncalomoreirasilva@gmail.com>
2024-10-22 11:34:59 +0100

    Add delete button which deletes the input created and the delete button when user clicks on it.

--- src/main/resources/static/js/buttonHandler.js
+++ src/main/resources/static/js/buttonHandler.js
@@ -2,8 +2,22 @@ document.getElementById('addButton').addEventListener('click', function() {
     const newInput = document.createElement("input");
     newInput.type = "text";
     newInput.placeholder = "Search for contact";
-    document.getElementById("inputDiv").appendChild(newInput);
     console.log("Add new input")
+
+    const deleteButton = document.createElement('deleteButton');
+    deleteButton.textContent = 'Delete';
+    deleteButton.type = 'button';
+    deleteButton.classList.add('btn', 'btn-danger', 'ml-2');
+    document.getElementById("inputDiv").appendChild(newInput);
+    document.getElementById("inputDiv").appendChild(deleteButton);
+
+    deleteButton.addEventListener("click", function(event) {
+        newInput.remove();
+        deleteButton.remove();
+    });
+
 });
+
+
 
 
